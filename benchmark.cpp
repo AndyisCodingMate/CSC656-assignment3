f6c8f24f269177d7a93f68221e010f0a4b2f03fa
Andy Poon <andypoon923@gemtrifi.com>
2024-05-01 05:57:40 -0700

    syntax improved

--- benchmark.cpp
+++ benchmark.cpp
@@ -54,7 +54,7 @@ bool check_accuracy(double *A, double *Anot, int nvalues)
 /* The benchmarking program */
 int main(int argc, char** argv) 
 {
-    std::cout << "Description:\t" << dgemv_desc << std::endl << std::endl;
+    std::cout << "\nDescription:\t" << dgemv_desc << std::endl << std::endl;
 
     std::cout << std::fixed << std::setprecision(5);
 
