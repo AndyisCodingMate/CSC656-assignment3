21c6d3543e749f04e88677e64bd34282fce89141
Andy Poon <andypoon923@gemtrifi.com>
2024-05-01 06:58:59 -0700

    fix vectorization

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,9 +8,11 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    for (int row = 0; row < n; row++) {
+      double sum = 0.0;
       for (int column = 0; column < n; column++) {
-         y[row] += A[(row * n) + column] * x[column];
+         sum += A[(row * n) + column] * x[column];
       }
+      y[row] = sum;
    }
 }
 
